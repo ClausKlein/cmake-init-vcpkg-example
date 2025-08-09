f2d20692782066e12fbda7f8d470ff2f2e9b0c55
ClausKlein <claus.klein@arcormail.de>
2025-08-09 19:50:57 +0200

    Format the code again

--- source/lib.cpp
+++ source/lib.cpp
@@ -1,24 +1,24 @@
 #include "lib.hpp"
 
-#include <fmt/format.h>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp> // for to_string
+#include <boost/uuid/uuid_io.hpp>  // for to_string
+#include <fmt/format.h>
 
 namespace
 {
 
 boost::uuids::uuid make_uuid()
 {
-    static boost::uuids::random_generator gen{};
-    return gen();
+  static boost::uuids::random_generator gen {};
+  return gen();
 }
 
-} // namespace
+}  // namespace
 
 library::library()
-    : name{ fmt::format("{}", "vcpkg-example") }
+    : name {fmt::format("{}", "vcpkg-example")}
 {
-    const auto uuid = make_uuid();
-    fmt::print("{}\n", boost::uuids::to_string(uuid));
+  const auto uuid = make_uuid();
+  fmt::print("{}\n", boost::uuids::to_string(uuid));
 }
